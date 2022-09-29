ce8de25a4291294897b220dcab638916329c5bc6
xingdong <xingdong@soyoung.com>
2022-09-29 16:37:28 +0800

    article36|unicode与字符编码

--- article29/q1/demo73.go
+++ article29/q1/demo73.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	str := "Go爱好者"
 	fmt.Printf("The string: %q\n", str)
-	fmt.Printf("  => runes(char): %q\n", []rune(str))
-	fmt.Printf("  => runes(hex): %x\n", []rune(str))
-	fmt.Printf("  => bytes(hex): [% x]\n", []byte(str))
+	fmt.Printf("  => runes(char): %q\n", []rune(str))   //%q
+	fmt.Printf("  => runes(hex): %x\n", []rune(str))    // %x 16进制
+	fmt.Printf("  => bytes(hex): [% x]\n", []byte(str)) //
 }
